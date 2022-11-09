f8c0967d688c8b22229353157a1627794a1b8003
geetanjalising <geetanjalisingh1815@gmail.com>
2022-11-10 03:21:59 +0530

    Updates

---
+++ precache-manifest.25816a4a34e4caaaa22ecfcb6c13f2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f0200ef38e5078f5e58",
+    "url": "/react-todo-app/static/css/main.ad9c883f.chunk.css"
+  },
+  {
+    "revision": "6f0200ef38e5078f5e58",
+    "url": "/react-todo-app/static/js/main.6f0200ef.chunk.js"
+  },
+  {
+    "revision": "e3587a82c9a2818d65df",
+    "url": "/react-todo-app/static/js/1.e3587a82.chunk.js"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "d9fe24634f9f0c89debb119bbe8caa38",
+    "url": "/react-todo-app/index.html"
+  }
+];
